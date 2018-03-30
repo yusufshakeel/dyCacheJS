0933a0ebb0185f2aa272f9c237dc4d5983e036c8
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2018-03-30 22:46:43 +0530

    test update

--- test/src/js/dyCacheTest.js
+++ test/src/js/dyCacheTest.js
@@ -14,7 +14,23 @@
 
 /*! dyCacheJS | (c) 2018 Yusuf Shakeel | https://github.com/yusufshakeel/dyCacheJS */
 
-const assert = require('chai').assert;
+const assert = chai.assert;
 
-describe('Testing dyCacheJS', function () {
+describe('Testing dyCacheJS', function() {
+
+    let obj;
+
+    beforeEach(function(){
+        obj = new dyCache();
+    });
+
+    it('should assert that obj is an instance of dyCacheJS class.', function () {
+        assert.instanceOf(obj, dyCache, 'obj is instance of dyCacheJS class');
+    });
+
+    it('should set key num to 10 and then assert that it is equal to 10.', function () {
+        obj.set('num', 10);
+        assert.equal(10, obj.get('num'));
+    });
+
 });
